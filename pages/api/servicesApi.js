a3b98a1e52145269ed8ad9fe52fa60aba98596b1
sluna10 <sluna@icstars.org>
2022-07-11 14:45:06 -0500

    update

--- pages/api/servicesApi.js
+++ pages/api/servicesApi.js
@@ -1,5 +1,5 @@
 // pages/api/products.js
-import prisma2 from "../../lib/prisma1";
+import prisma2 from "../../lib/prisma2";
 
 export default async function handle(req, res) {
   const Services = await prisma2.Services.findMany();
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import { ArticleList } from '../components/ArticleList'
 
 export default function Home() {
   return (
